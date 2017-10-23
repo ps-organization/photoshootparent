518a051df5c999e6acb4ab194045e4d506b08996
admin <jdzgd@qq.com>
2017-10-23 09:44:09 +0800

    new branch

--- photoshoot/src/main/java/com/instrantes/test.java
+++ photoshoot/src/main/java/com/instrantes/test.java
@@ -1,7 +1,13 @@
 package com.instrantes;
 
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class test {
     public static void main(String[] args) {
@@ -16,6 +22,17 @@ public class test {
 //        String str = "http://localhost:8080/upload/images/2017/09/24/045543511.jpg";
 //        String aStr=str.substring(str.length()-24);
 //        System.out.println(aStr);
+
+        String str="{\"title\":\"Morning\",\"name\":\"Tom\"}";
+
+//        JSONArray list= JSONObject.parseArray(str);
+//        System.out.println("json:"+list.get(1));
+        Map<String,Object> map1 = (Map<String,Object>)JSONObject.parse(str);
+
+//        System.out.println(map1));
+//        List listl = new ArrayList();
+//        listl = JSONObject.parseArray(str);
+//        System.out.println("json2:"+listl.get(1));
 
     }
 
