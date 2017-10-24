8ee122297fe84cded3836a6384c749f36da26398
fatgui <852786032@qq.com>
2017-10-24 12:53:35 +0800

    resetpw

--- photoshoot/src/main/webapp/static/js/resetpw.js
+++ photoshoot/src/main/webapp/static/js/resetpw.js
@@ -1,13 +1,14 @@
+/*手机验证*/
 $().ready(function () {
     $('#rgs-get-code').on('click', function() {
         var _this = this;
         var loopTime = 59;
         // 修改样式
-        $(_this).prop('disabled', true).removeClass('aui-btn-warning').html('重新获取(60s)');
+        $(_this).prop('disabled', true).removeClass('aui-btn-warning').addClass('rgs-get-code').html('重新获取(60s)');
         // 定时器
         var countDown = setInterval(function() {
             if (loopTime == 0) {
-                $(_this).prop('disabled', false).addClass('aui-btn-warning').html('获取验证码');
+                $(_this).prop('disabled', false).removeClass('rgs-get-code').addClass('aui-btn-warning').html('获取验证码');
                 // 清除定时器
                 window.clearInterval(countDown);
             }else {
