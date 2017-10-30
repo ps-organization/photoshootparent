4b54fd4de800185289ec2595b858d75a2f406ae3
admin <jdzgd@qq.com>
2017-10-30 15:49:56 +0800

    complete the like function

--- photoshoot/src/main/java/com/instrantes/controller/PsLikeController.java
+++ photoshoot/src/main/java/com/instrantes/controller/PsLikeController.java
@@ -3,30 +3,30 @@ package com.instrantes.controller;
 import com.instrantes.service.PsLikeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 /**
  * Created by Lime on 2017/10/30
  */
 @Controller
-@RequestMapping("/Like")
+@RequestMapping("/like")
 public class PsLikeController {
     @Autowired
     PsLikeService psLikeService;
 
-    @RequestMapping(value = "/Like",method = RequestMethod.POST)
+    @RequestMapping(value = "/likeRecord", method = RequestMethod.POST)
     /**
     *当用户点赞时，触发该功能，插入一条点赞记录
-    *@param [likeCollectionid, likeUserid]
+    *@param [likeJson]
     *@return void
     *@date 2017/10/30
     */
     @ResponseBody
-    public void insertLikeRecordByUserId(Integer likeCollectionid, Integer likeUserid) {
-        System.out.println("likeCollection:"+likeCollectionid);
-        System.out.println("likeUserid:"+likeUserid);
-        psLikeService.insertLikeRecordByUserId(likeCollectionid, likeUserid);
+    public void insertLikeRecordByUserId(@RequestBody Map<String, Integer> likeJson) {
+//                Map<String,String>map = JSON.parseObject(likeJson,new TypeReference<Map<String, String>>(){});
+        System.out.println("likeCollection:" + likeJson.get("likeCollectionid"));
+        psLikeService.insertLikeRecordByUserId(likeJson.get("likeCollectionid"), likeJson.get("likeUserid"));
     }
 }
--- photoshoot/src/main/java/com/instrantes/dao/PsLikeDao.java
+++ photoshoot/src/main/java/com/instrantes/dao/PsLikeDao.java
@@ -1,6 +1,7 @@
 package com.instrantes.dao;
 
 import com.instrantes.pojo.PsLike;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,6 +12,6 @@ public interface PsLikeDao{
     PsLike showLikeStatus(Integer likeUserid, Integer collectionId);//    1.显示自己是否赞,若无当前用户赞的相关记录,insertPsLike
     void insertPsLike(Integer likeStatus,Integer likeUserid);//    2.插入当前用户的空白赞记录，用getter获取记录id
     void updateSingleCollectionLikeStatus(Integer likeId,Integer likeStatus);//    点赞/消赞
-    void insertLikeRecordByUserId(Integer likeCollectionid,Integer likeUserid); //插入点赞记录，
+    void insertLikeRecordByUserId(@Param("likeCollectionid") Integer likeCollectionid, @Param("likeUserid") Integer likeUserid); //插入点赞记录，
 
 }
