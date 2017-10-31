17b2dae2d9f7054de8e6f38046aaf2cc49ea6e5a
CirnoBreak <1103054869@qq.com>
2017-10-31 18:14:28 +0800

    thumbUp

--- photoshoot/src/main/webapp/static/js/picShow.js
+++ photoshoot/src/main/webapp/static/js/picShow.js
@@ -2,7 +2,52 @@ $().ready(function () {
     /*console.log($('#t-img').find('img').width());
     console.log($('#t-img img').height());*/
     //此处是全部图片加载的功能
-    imgLoad();
+    $.post("/collection/allCollection", function (data) {
+        for (var i = 0; i < data.length; i++) {
+            /*console.log("id:" + data[i].collectionId + ";location:" + data[i].collectionPhotolocation);
+            console.log("userName:" + data[i].userName + ";likeCount:" + data[i].likeCount);
+            console.log("collectionPhotoname:" + data[i].collectionPhotoname);*/
+            $('ul').append("<li>\n" +
+                "            <figure>\n" +
+                "                <a href=\"https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-575188.png\" target=\"_new\">\n" +
+                "                    <div class='pic'>\n" +
+                "                        <img class='lazyload' data-src='../upload/" + data[i].collectionPhotolocation + "' alt=''>\n" +
+                "                    </div>\n" +
+                "                </a>\n" +
+                "                <figcaption>\n" +
+                "                    <h3><a href=\"http://www.baidu.com\" target='_new'>" + data[i].collectionPhotoname + "</a></h3>\n" +
+                "                    <div class='status'>\n" +
+                "                        <div class='author'>\n" +
+                "                            <a href='#'><img src='' alt=''><span>" + data[i].userName + "</span></a>\n" +
+                "                        </div>\n" +
+                "                        <div class=\"thumbUp\">\n" +
+                "                            <a href='javascript:void(0);' class='like'><svg t=\"1509418554370\" class=\"icon\" style=\"\" viewBox=\"0 0 1024 1024\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" p-id=\"2688\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width=\"200\" height=\"200\"><defs><style type=\"text/css\"></style></defs><path d=\"M706.56 136.533333c-75.093333 0-150.186667 40.96-194.56 98.986667C467.626667 174.08 392.533333 136.533333 317.44 136.533333c-129.706667 0-225.28 95.573333-225.28 225.28 0 157.013333 136.533333 283.306667 368.64 491.52l34.133333 30.72C498.346667 887.466667 505.173333 887.466667 512 887.466667s13.653333-3.413333 20.48-6.826667l34.133333-30.72c228.693333-208.213333 368.64-334.506667 368.64-491.52C931.84 232.106667 836.266667 136.533333 706.56 136.533333z\" p-id=\"2689\" fill=\"#8a8a8a\"></path></svg><span>" + data[i].likeCount + "</span></a>\n" +
+                "                        </div>\n" +
+                "                    </div>\n" +
+                "                </figcaption>\n" +
+                "            </figure>\n" +
+                "        </li>");
+            console.log(data[i].likeStatus);
+        }
+        //添加多次点赞提示
+        $('ul').append("<p class=\"tip\"><span>认真点的赞不能取消哟</span></p>");
+        //点赞功能的实现
+        console.log($('.like').length);
+        $('.like').on('click', function() {
+            console.log('okok');
+            var that = $(this);
+            var svgColor = $(this).find('svg path').attr('fill');
+            console.log($(this).find('svg path').attr('fill'));
+            if (svgColor == '#8a8a8a') {
+                //改变点赞图标颜色，并给点赞数+1
+                addLike(that);
+                //插入点赞记录的ajax
+
+            } else if(svgColor == 'blue'){
+                noRepeatLike(that);
+            }
+        });
+    });
     /*var count = 0;
     $('img').on('load', function() {
         count++;
@@ -11,6 +56,7 @@ $().ready(function () {
             imgLoad();
         }
     });*/
+
 
 });
 
@@ -34,8 +80,8 @@ function imgLoad() {
                 "                        <div class='author'>\n" +
                 "                            <a href='#'><img src='' alt=''><span>"+data[i].userName+"</span></a>\n" +
                 "                        </div>\n" +
-                "                        <div class=\"collection\">\n" +
-                "                            <a href='#'><img src='../img/common/Collection.svg' alt=''><span>"+data[i].likeCount+"</span></a>\n" +
+                "                        <div class=\"thumbUp\">\n" +
+                "                            <a href='javascript:void(0);' class='like'><svg t=\"1509418554370\" class=\"icon\" style=\"\" viewBox=\"0 0 1024 1024\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" p-id=\"2688\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width=\"200\" height=\"200\"><defs><style type=\"text/css\"></style></defs><path d=\"M706.56 136.533333c-75.093333 0-150.186667 40.96-194.56 98.986667C467.626667 174.08 392.533333 136.533333 317.44 136.533333c-129.706667 0-225.28 95.573333-225.28 225.28 0 157.013333 136.533333 283.306667 368.64 491.52l34.133333 30.72C498.346667 887.466667 505.173333 887.466667 512 887.466667s13.653333-3.413333 20.48-6.826667l34.133333-30.72c228.693333-208.213333 368.64-334.506667 368.64-491.52C931.84 232.106667 836.266667 136.533333 706.56 136.533333z\" p-id=\"2689\" fill=\"#8a8a8a\"></path></svg><span>"+data[i].likeCount+"</span></a>\n" +
                 "                        </div>\n" +
                 "                    </div>\n" +
                 "                </figcaption>\n" +
@@ -44,4 +90,23 @@ function imgLoad() {
             console.log(data[i].likeStatus);
         }
     });
+
+
+}
+
+function addLike(e) {
+    var likeCount=e.find('span');
+    e.find('svg').stop().animate({width:'0',height: '0'},50);
+    e.find('svg').animate({width:'18px',height: '18px'},'slow').children().attr({fill: 'blue'});
+    likeCount.html(parseInt(likeCount.html())+1);
+}
+function noRepeatLike(e) {
+    var posLeft = e.parents('li').offset().left;
+    var disLeft=($('.tip').outerWidth()-e.parents('li').outerWidth())/2;
+    var l = posLeft-disLeft;
+    var posTop = e.parents('li').offset().top;
+    var disTop=($('.tip').outerHeight()-e.parents('li').outerHeight())/2;
+    var t = posTop - disTop;
+    // console.log(disLeft);
+    $('.tip').css({'left':l+'px','top': t+'px'}).stop(true, true).fadeIn(500).fadeOut(1500);
 }
