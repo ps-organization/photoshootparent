720c342c3fb97a502fd1d6c702c06101d11ad4c6
admin <jdzgd@qq.com>
2017-11-28 21:43:10 +0800

    webSoceket

---
+++ ps-web/src/main/java/com/instrantes/netty/WebSocketServer.java
@@ -0,0 +1,12 @@
+package com.instrantes.netty;
+
+import io.netty.channel.EventLoopGroup;
+
+/**
+ * Created by Lime on 2017/11/28
+ */
+public class WebSocketServer {
+    public void start(int port)throws Exception{
+//        EventLoopGroup
+    }
+}
